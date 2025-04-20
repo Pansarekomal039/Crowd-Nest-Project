68f218ada7db70ad6bad834da33f534d22f5f730
Pansarekomal039 <pansarekomal39@gmail.com>
2025-04-20 11:18:08 +0530

    crowd-nest

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import RootStack from "../navigators/RootStack";
- 
+import messaging from '@react-native-firebase/messaging';
 
-function App() {
-  return (
-      <RootStack />
-  );
+function App() {  
+async function requestUserPermission(){
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+  authStatus == messaging.AuthorizationStatus.AUTHORIZED ||
+  authStatus == messaging.AuthorizationStatus.PROVISIONAL;
+
+  if(enabled) {
+    console.log('Authorization status:',authStatus);
+  }
 }
 
+const getToken = async() => {
+  const token = await messaging().getToken()
+  console.log("Token = ", token)
+}
+
+useEffect(() => {
+  requestUserPermission()
+  getToken()
+
+},[])
+
+
+return (
+  <RootStack />
+);
+}
 export default App;  
