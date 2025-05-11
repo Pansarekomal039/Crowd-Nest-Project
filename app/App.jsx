982e8d308b513b1507ef573e6ee986197ca7ef43
Pansarekomal039 <pansarekomal39@gmail.com>
2025-05-11 10:14:19 +0530

    Final Commit

--- app/App.jsx
+++ app/App.jsx
@@ -1,33 +1,9 @@
+// App.jsx
 import React from "react"; 
 import RootStack from "../navigators/RootStack";
-import messaging from '@react-native-firebase/messaging';
 
-function App() {  
-async function requestUserPermission(){
-  const authStatus = await messaging().requestPermission();
-  const enabled =
-  authStatus == messaging.AuthorizationStatus.AUTHORIZED ||
-  authStatus == messaging.AuthorizationStatus.PROVISIONAL;
-
-  if(enabled) {
-    console.log('Authorization status:',authStatus);
-  }
-} 
-
-const getToken = async() => {
-  const token = await messaging().getToken()
-  console.log("Token = ", token)
+const App = () => {  
+  return <RootStack />;
 }
 
-useEffect(() => {
-  requestUserPermission()
-  getToken()
-
-},[])
-
-
-return (
-  <RootStack />
-);
-}
-export default App;  
+export default App;
